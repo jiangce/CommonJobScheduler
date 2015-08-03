d1fc9a73b13f14c1c71257e6fc99a5263e29a660
jiangce <jiangce@togeek.cn>
2015-08-04 00:39:28 +0800

    修改

--- setup.py
+++ setup.py
@@ -10,16 +10,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['apscheduler'],
-    entry_points={
-        # 'console_scripts': [
-        #     'foo = my_package.some_module:main_func',
-        #     'bar = other_module:some_func',
-        # ],
-        # 'gui_scripts': [
-        #     'jsw = job_service:start_window',
-        # ]
-        'console_scripts': [
-            'jsw = job_service:main',
-        ]
-    }
+    entry_points={'gui_scripts': ['jsw = job_service:main', ]}
 )
